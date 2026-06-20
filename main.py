c78c6a900c5363edbaef0a8efd4522bea1be42c7
anasarahlinhares-ux <anasarahlinhares@gmail.com>
2026-06-20 15:15:13 -0300

    adicionando endepoint do autor e API REST

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 from controllers.auth_controller import auth_bp
 from models.user import User
 from models.equipamento import Equipamento
@@ -6,6 +6,8 @@ from models import db
 import os
 
 app = Flask(__name__)
+
+app.json.ensure_ascii = False
 
 # Configurações
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
@@ -17,6 +19,11 @@ db.init_app(app)
 
 with app.app_context():
     db.create_all()
+
+    if not User.query.filter_by(email="admin@email.com").first():
+        user = User(email="admin@email.com", password="1234")
+        db.session.add(user)
+        db.session.commit()
 
 # 🔥 SÓ REGISTRA O BLUEPRINT
 app.register_blueprint(auth_bp)
@@ -31,6 +38,26 @@ def index():
         db.session.add(user)
         db.session.commit()
 
+@app.route("/autor")
+def autor():
+    return jsonify({
+        "nome": "Ana Sarah Anastacio Linhares",
+        "curso": "Técnico em Informática para Internet",
+        "disciplina": "Desenvolvimento Web II",
+        "projeto": "InventaTI"
+    })
+
+@app.route("/api/usuarios")
+def api_usuarios():
+    usuarios = User.query.all()
+
+    return jsonify([
+        {
+            "id": usuario.id,
+            "email": usuario.email
+        }
+        for usuario in usuarios
+    ])
 
 
 if __name__ == "__main__":
